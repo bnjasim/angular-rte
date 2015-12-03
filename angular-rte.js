4c4fa3263b8b9e52de982a2bc5767bf7a23a2437
binu <bnjasim@gmail.com>
2015-12-03 10:58:53 +0530

    Basic icons setup Copy the passed text content to the text-area

--- angular-rte.js
+++ angular-rte.js
@@ -1,5 +1,23 @@
 angular.module('main', [])
 
 .directive('angularRte', function() {
-	scope: {}
+  return {
+	restrict: 'E',
+	replace: true,
+	scope: {content: '=textContent'},
+	templateUrl: 'rte.html',
+	controller: ['$scope', function($scope) {
+		$scope.content = 'Controller!';
+		console.log($scope.content);
+	}],
+
+	link: function(scope, element, attrs) {
+		var text_area = element.children().eq(1);
+		var icon_area = element.children().eq(0);
+		// copy the passed content to the text-area
+		text_area.html(scope.content);
+
+
+	}
+  }
 })
