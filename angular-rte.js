eba15f79326c5aa016e24a8205786fdb8815251c
binu <bnjasim@gmail.com>
2015-12-03 11:51:31 +0530

    Basic commands like bold italic etc. using execCommand

--- angular-rte.js
+++ angular-rte.js
@@ -9,14 +9,21 @@ angular.module('main', [])
 	controller: ['$scope', function($scope) {
 		$scope.content = 'Controller!';
 		console.log($scope.content);
+
+		
 	}],
 
 	link: function(scope, element, attrs) {
-		var text_area = element.children().eq(1);
+		var text_area = element.children().eq(2);
 		var icon_area = element.children().eq(0);
 		// copy the passed content to the text-area
 		text_area.html(scope.content);
 
+		scope.runCommand = function(command) {
+			text_area[0].focus();
+			document.execCommand(command, false);
+			text_area[0].focus();
+		}
 
 	}
   }
