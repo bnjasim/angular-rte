0d1ebc16bf1d9375ce14ee1ed971c686490900d9
binu <bnjasim@gmail.com>
2015-12-03 19:07:54 +0530

    Hilite color and Headings h1, h2, h3
    h1, h2, h3 with the same button with toggle only if clicked wihtin 2 seconds

--- angular-rte.js
+++ angular-rte.js
@@ -14,6 +14,14 @@ angular.module('main', [])
 	}],
 
 	link: function(scope, element, attrs) {
+		// set initial values
+		var value = '';
+		var hilite_state = false;
+		var headings = ['H1', 'H2', 'H3', 'A'];
+		var h_index = 0;
+		scope.h = 'H1';
+		var last_clicked = new Date().getTime();
+
 		var text_area = element.children().eq(2);
 		var icon_area = element.children().eq(0);
 		// copy the passed content to the text-area
@@ -21,7 +29,35 @@ angular.module('main', [])
 
 		scope.runCommand = function(command) {
 			text_area[0].focus();
-			document.execCommand(command, false);
+			
+			if (command === 'hiliteColor') {
+				value = '#FFFBCC';
+				hilite_state = !hilite_state;
+
+				if (!hilite_state)
+					value = '#FFF';
+
+			}
+
+			if (command === 'formatBlock') {
+				// increment h_index only if last_clicked is within 2 seconds
+				if ( (new Date().getTime() - last_clicked) < 2000 ) {
+					h_index = h_index===3?0:h_index+1;	
+					// change the button on the icon-group
+					scope.h = headings[h_index];
+				}
+
+				last_clicked = new Date().getTime();
+
+				// H1 is actually h2, H2 is h3 etc.
+				if (h_index === 0)  value = 'h2';
+				if (h_index === 1)  value = 'h3';
+				if (h_index === 2)  value = 'h4';
+				if (h_index === 3)  value = 'div';
+
+			}
+
+			document.execCommand(command, false, value);
 			text_area[0].focus();
 		}
 
